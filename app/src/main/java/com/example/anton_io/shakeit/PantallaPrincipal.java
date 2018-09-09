fd5154d8d623e409cb974f51ca4d3d4fd7e05ce4
antonio072 <16030681@itcelaya.edu.mx>
2018-09-08 23:48:02 -0500

    Initial commit

--- app/src/main/java/com/example/anton_io/shakeit/PantallaPrincipal.java
+++ app/src/main/java/com/example/anton_io/shakeit/PantallaPrincipal.java
@@ -2,22 +2,25 @@ package com.example.anton_io.shakeit;
 
 import android.content.Context;
 import android.hardware.Sensor;
-import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
+import android.widget.*;
+
 public class PantallaPrincipal extends AppCompatActivity {
-
-
+    //TODO cambiar icono
+    int i=0;
     // Para detectar el shakeo
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     private SensorDetect mShakeDetector;
 
-
+    String[] respuestas=new String[]{"Si",
+                                    "No",
+                                    "Tal vez",
+                                    "Por supuesto"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,18 +34,40 @@ public class PantallaPrincipal extends AppCompatActivity {
 
             @Override
             public void onShake(int count) {
-                Toast.makeText(PantallaPrincipal.this, "Se movio alv", Toast.LENGTH_SHORT).show();
+                TextView txt=findViewById(R.id.Txt_Mensaje);
+                int aleatorio=nAleatorio();
+                try {
+                    if(aleatorio==0 ||aleatorio==4 || aleatorio==8)
+                        txt.setText(respuestas[0]);
+                    else
+                    if(aleatorio==1 || aleatorio==5 || aleatorio==9)
+                        txt.setText(respuestas[1]);
+                    else
+                    if(aleatorio==2 || aleatorio==6)
+                        txt.setText(respuestas[2]);
+                    else
+                    if(aleatorio==3 || aleatorio==7 )
+                        txt.setText(respuestas[3]);
+
+
+                } catch (Exception e) {
+                }
             }
         });
+    }
 
+    int nAleatorio(){
+        String respAleatoria;
+        respAleatoria=""+System.nanoTime();
+       String cAt=""+respAleatoria.charAt(respAleatoria.length()-2);
+
+       return Integer.parseInt(cAt);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        // Add the following line to register the Session Manager Listener onResume
         mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
-        Toast.makeText(PantallaPrincipal.this, "Se movio alv", Toast.LENGTH_SHORT).show();
 
     }
     @Override
@@ -50,6 +75,7 @@ public class PantallaPrincipal extends AppCompatActivity {
         // Add the following line to unregister the Sensor Manager onPause
         mSensorManager.unregisterListener(mShakeDetector);
         super.onPause();
+
 
     }
 }
